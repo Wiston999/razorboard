2fcad5eeeac23934a9a7d19d2fdc0b254f8fd0e7
Victor Cabezas <vcabezas@tuenti.com>
2020-10-24 00:18:28 +0200

    Use same tag color scheme in tags list view

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 - Autogenerate docker image internal version on `npm build`
 - Restore error handling of polled HTTP requests
+- Add tag list view colored tags
 
 ## [0.2.0] - 2020-10-23
 ### Added
--- razorboard/src/app/tag-list-item/tag-list-item.component.ts
+++ razorboard/src/app/tag-list-item/tag-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ColorTagService } from '../color-tag.service';
 
 @Component({
   selector: '[tag-list-item]',
@@ -9,7 +10,9 @@ export class TagListItemComponent implements OnInit {
   @Input() tag;
   @Input() columns: string[];
 
-  constructor() { }
+  constructor(
+    public colorTag: ColorTagService,
+  ) { }
 
   ngOnInit() {
   }
