e939775b2482ee9c57da8c14ba5ab0001f6ec790
Victor Cabezas <vcabezas@tuenti.com>
2020-11-02 21:01:31 +0100

    node-detail coverage to 80%

--- razorboard/src/app/node-detail/node-detail.component.spec.ts
+++ razorboard/src/app/node-detail/node-detail.component.spec.ts
@@ -8,6 +8,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ActivatedRouteStub } from '../../testing/activated-route-stub';
 import { RazorapiServiceStub } from '../../testing/razorapi.service.stub';
@@ -37,6 +38,7 @@ describe('NodeDetailComponent', () => {
         ToastrModule.forRoot(),
         RouterTestingModule,
         HttpClientTestingModule,
+        NoopAnimationsModule,
       ],
       providers: [
         NodeDetailComponent,
@@ -85,5 +87,98 @@ describe('NodeDetailComponent', () => {
     const content = fixture.nativeElement.querySelectorAll('.fact-value');
     const count = Object.values(nodeObj.facts).length;
     expect(content.length).toBe(count);
+  });
+
+  it('should start edit mode', () => {
+    component.editStart();
+    fixture.detectChanges();
+    const editContent = fixture.nativeElement.querySelectorAll('.metadata-edit');
+    expect(component.editMode).toBe(true);
+    expect(component.node.metadata).toEqual(component.editNode.metadata);
+    expect(editContent.length).toBe(Object.values(component.editNode.metadata).length);
+  });
+
+  it('should remove metadata', () => {
+    const deletedKey = Object.keys(component.node.metadata)[0];
+    component.editStart();
+    component.metadataDelete(deletedKey);
+    fixture.detectChanges();
+    const editContent = fixture.nativeElement.querySelectorAll('.metadata-edit');
+    expect(editContent.length).toBe(Object.values(component.editNode.metadata).length);
+    expect(component.editNode.metadata[deletedKey]).toBeUndefined();
+  });
+
+  it('should add metadata', fakeAsync(() => {
+    const currentMetadata = Object.keys(component.node.metadata).length;
+    component.editStart();
+    component.newMetadataKey = 'zzzzztest-key';
+    component.newMetadataValue = 'test-value';
+    component.metadataAdd();
+    fixture.detectChanges();
+    tick();
+    const editLabels = fixture.nativeElement.querySelectorAll('.metadata-edit-label');
+    const editContent = fixture.nativeElement.querySelectorAll('.metadata-edit');
+    let newIndex: number;
+    editLabels.forEach((el, i) => {
+      if (el.textContent === 'zzzzztest-key') {
+        newIndex = i;
+      }
+    });
+    expect(component.editNode.metadata['zzzzztest-key']).toBe('test-value');
+    expect(currentMetadata + 1).toBe(Object.values(component.editNode.metadata).length);
+    expect(editContent.length).toBe(Object.values(component.editNode.metadata).length);
+    expect(editContent[newIndex].value).toBe('test-value');
+  }));
+
+  it('should modify metadata', fakeAsync(() => {
+    const currentMetadata = Object.keys(component.node.metadata).length;
+    const metadataKey = Object.keys(component.node.metadata)[0];
+    component.editStart();
+    component.metadataChange(metadataKey, 'new-test-value');
+    fixture.detectChanges();
+    tick();
+    const editLabels = fixture.nativeElement.querySelectorAll('.metadata-edit-label');
+    const editContent = fixture.nativeElement.querySelectorAll('.metadata-edit');
+    let modifiedIndex: number;
+    editLabels.forEach((el, i) => {
+      if (el.textContent === metadataKey) {
+        modifiedIndex = i;
+      }
+    });
+    expect(currentMetadata).toBe(Object.values(component.editNode.metadata).length);
+    expect(component.editNode.metadata[metadataKey]).toBe('new-test-value');
+    expect(editContent[modifiedIndex].value).toBe('new-test-value');
+  }));
+
+  it('should save metadata', () => {
+    const modifyKey = Object.keys(component.node.metadata)[0];
+    const deleteKey = Object.keys(component.node.metadata)[1];
+    component.editStart();
+    // Modify metadata
+    component.metadataChange(modifyKey, 'new-test-value');
+    // Delete metadata
+    component.metadataDelete(deleteKey);
+    // Add new metadata
+    component.newMetadataKey = 'zzzzztest-key';
+    component.newMetadataValue = 'test-value';
+    component.metadataAdd();
+    // Perform save
+    component.metadataSave('save');
+    fixture.detectChanges();
+    const apiRequest = razorApiStub.getLastRequest();
+    const updateMap = {'zzzzztest-key': 'test-value'};
+    updateMap[modifyKey] = 'new-test-value';
+    expect(apiRequest.id).toBe(component.node.name);
+    expect(apiRequest.update).toEqual(updateMap);
+    expect(apiRequest.remove).toEqual([deleteKey]);
+    expect(component.editMode).toBeFalsy();
+  });
+
+  it('should not save metadata', () => {
+    component.editStart();
+    // Perform save
+    component.metadataSave('other-than-save');
+    fixture.detectChanges();
+    expect(razorApiStub.getRequests().length).toBe(0);
   });
 });
--- razorboard/src/testing/razorapi.service.stub.ts
+++ razorboard/src/testing/razorapi.service.stub.ts
@@ -4,6 +4,8 @@ import { Observable, ReplaySubject, of } from 'rxjs';
 
 export class RazorapiServiceStub {
 
+  private requests = [];
+
   reload$ = new Observable();
 
   getNodes() {
@@ -17,5 +19,24 @@ export class RazorapiServiceStub {
     const nodeObjs = nodeListResponse.items.filter(n => n.name === id);
     subject.next(nodeObjs[0]);
     return subject;
+  }
+
+  modifyNodeMetadata(id: string, update: {string: any}, remove: string[]) {
+    this.requests.push({id, update, remove});
+    return of(true);
+  }
+
+  reinstallNode(id: string, keepPolicy: boolean) {
+    this.requests.push({id, keepPolicy});
+    return of(true);
+  }
+
+  // Testing utils
+  getRequests() {
+    return this.requests;
+  }
+
+  getLastRequest() {
+    return this.requests[this.requests.length - 1];
   }
 }
