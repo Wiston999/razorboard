0af31d297640622d6926fc96c0df3ec846eeae04
Victor Cabezas <vcabezas@tuenti.com>
2020-11-12 22:46:38 +0100

    Add tests for rainbow brackets

--- razorboard/src/app/rainbow-brackets.pipe.spec.ts
+++ razorboard/src/app/rainbow-brackets.pipe.spec.ts
@@ -1,8 +1,36 @@
 import { RainbowBracketsPipe } from './rainbow-brackets.pipe';
 
 describe('RainbowBracketsPipe', () => {
+  const pipe = new RainbowBracketsPipe();
+
   it('create an instance', () => {
-    const pipe = new RainbowBracketsPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should not mark un-bracketed', () => {
+    const text = 'this is a test text with no brackets';
+    expect(pipe.transform(text)).toEqual(text);
+  });
+
+  it('should not mark unbalanced bracketed', () => {
+    let text = 'this is a test [ { (text ( [ { with unbalanced brackets';
+    expect(pipe.transform(text)).toEqual(text);
+    text = 'this is a test [ { (text] ) } with other unbalanced brackets';
+    expect(pipe.transform(text)).toEqual(text);
+    text = '}this is a test text with other unbalanced brackets';
+    expect(pipe.transform(text)).toEqual(text);
+  });
+
+  it('should mark brackets properly', () => {
+    const text = '[this is a (test) text with {balanced} [brackets]]';
+    const expected = '\
+<span class="rb-1">[</span>\
+this is a \
+<span class="rb-2">(</span>test<span class="rb-2">)</span> \
+text with \
+<span class="rb-2">{</span>balanced<span class="rb-2">}</span> \
+<span class="rb-2">[</span>brackets<span class="rb-2">]</span>\
+<span class="rb-1">]</span>';
+    expect(pipe.transform(text)).toEqual(expected);
   });
 });
