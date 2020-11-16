56b3ad2af791b155a8c33acac007f6f5f327338d
Victor Cabezas <vcabezas@tuenti.com>
2020-11-16 21:21:48 +0100

    Add more tests to razorapi

--- razorboard/src/app/razorapi.service.spec.ts
+++ razorboard/src/app/razorapi.service.spec.ts
@@ -8,9 +8,75 @@ import { LocalStorageStub } from '../testing/local-storage.stub';
 
 import { RazorapiService } from './razorapi.service';
 
+// This object is tests with and without auth header, to test a new
+// function, use as key the collection name and value the function implemented
+const collections = {
+  nodes: (svc) => svc.getNodes(),
+  hooks: (svc) => svc.getHooks(),
+  policies: (svc) => svc.getPolicies(),
+  tags: (svc) => svc.getTags(),
+  tasks: (svc) => svc.getTasks(),
+  events: (svc) => svc.getEvents(),
+  brokers: (svc) => svc.getBrokers(),
+  repos: (svc) => svc.getRepos(),
+  commands: (svc) => svc.getCommands(),
+  config: (svc) => svc.getConfiguration(),
+  'nodes\\/node1': (svc) => svc.getNode('node1'),
+  'nodes\\/node1\\/log': (svc) => svc.getNodeLogs('node1'),
+};
+
+const commandCases = {
+  'reinstall-node-keep': {
+    command: 'reinstall-node',
+    req: (svc) => svc.reinstallNode('node1', true),
+    check: (req) => {
+      expect(req.body.name).toEqual('node1');
+      expect(req.body.same_policy).toBeTruthy();
+    },
+  },
+  'reinstall-node-no-keep': {
+    command: 'reinstall-node',
+    req: (svc) => svc.reinstallNode('node1', false),
+    check: (req) => {
+      expect(req.body.name).toEqual('node1');
+      expect(req.body.same_policy).toBeFalsy();
+    },
+  },
+  'modify-metadata-full-data': {
+    command: 'modify-node-metadata',
+    req: (svc) => svc.modifyNodeMetadata('node1', {key1: 'value'}, ['key2']),
+    check: (req) => {
+      expect(req.body.node).toEqual('node1');
+      expect(req.body.update.key1).toEqual('value');
+      expect(req.body.remove.length).toEqual(1);
+      expect(req.body.remove[0]).toEqual('key2');
+    },
+  },
+  'modify-metadata-no-remove': {
+    command: 'modify-node-metadata',
+    req: (svc) => svc.modifyNodeMetadata('node1', {key1: 'value'}, []),
+    check: (req) => {
+      expect(req.body.node).toEqual('node1');
+      expect(req.body.update.key1).toEqual('value');
+      expect(req.body.remove.length).toEqual(0);
+    },
+  },
+  'modify-metadata-no-update': {
+    command: 'modify-node-metadata',
+    req: (svc) => svc.modifyNodeMetadata('node1', {}, ['key2']),
+    check: (req) => {
+      expect(req.body.node).toEqual('node1');
+      expect(Object.keys(req.body.update).length).toEqual(0);
+      expect(req.body.remove.length).toEqual(1);
+      expect(req.body.remove[0]).toEqual('key2');
+    },
+  },
+};
+
 describe('RazorapiService', () => {
   let service: RazorapiService;
   let localStorageStub: LocalStorageStub;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     localStorageStub = new LocalStorageStub();
@@ -23,6 +89,7 @@ describe('RazorapiService', () => {
     });
     localStorageStub.clear();
     service = TestBed.get(RazorapiService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
@@ -61,4 +128,58 @@ describe('RazorapiService', () => {
     service.connect(service.getEndpoint(), '', '', 100000, true);
     expect(service.connect(service.getEndpoint(), '', '', 100000, true)).toBeFalsy();
   }));
+
+  Object.keys(collections).forEach((key) => {
+    it(`should get collection ${key} without AUTH`, fakeAsync(() => {
+      service.connect('http://localhost', '', '', 100, false);
+      collections[key](service).subscribe(response => {
+        expect(response).toBeTruthy();
+      }).unsubscribe();
+      tick();
+
+      const httpRequest = httpMock.match((req) => new RegExp(`api\/collections\/${key}?.+`).test(req.url))[0];
+      expect(httpRequest.request.headers.has('Authorization')).toBeFalsy();
+    }));
+
+    it(`should get collection ${key} with AUTH`, fakeAsync(() => {
+      const header = 'Basic dGVzdC11c2VyOnRlc3QtcHdk';
+      service.connect('http://localhost', 'test-user', 'test-pwd', 100, false);
+      collections[key](service).subscribe(response => {
+        expect(response).toBeTruthy();
+      }).unsubscribe();
+      tick();
+
+      const httpRequest = httpMock.match((req) => new RegExp(`api\/collections\/${key}?.+`).test(req.url))[0];
+      expect(httpRequest.request.headers.get('Authorization')).toEqual(header);
+    }));
+  });
+
+  Object.keys(commandCases).forEach((key) => {
+    it(`should send command ${key} without AUTH`, fakeAsync(() => {
+      const reqInfo = commandCases[key];
+      service.connect('http://localhost', '', '', 100, false);
+      reqInfo.req(service).subscribe(response => {
+        expect(response).toBeTruthy();
+      }).unsubscribe();
+      tick();
+
+      const httpRequest = httpMock.match((req) => new RegExp(`api\/commands\/${reqInfo.command}?.+`).test(req.url))[0];
+      expect(httpRequest.request.headers.has('Authorization')).toBeFalsy();
+      reqInfo.check(httpRequest.request);
+    }));
+
+    it(`should send command ${key} with AUTH`, fakeAsync(() => {
+      const reqInfo = commandCases[key];
+      const header = 'Basic dGVzdC11c2VyOnRlc3QtcHdk';
+      service.connect('http://localhost', 'test-user', 'test-pwd', 100, false);
+      reqInfo.req(service).subscribe(response => {
+        expect(response).toBeTruthy();
+      }).unsubscribe();
+      tick();
+
+      const httpRequest = httpMock.match((req) => new RegExp(`api\/commands\/${reqInfo.command}?.+`).test(req.url))[0];
+      expect(httpRequest.request.headers.get('Authorization')).toEqual(header);
+      reqInfo.check(httpRequest.request);
+    }));
+  });
 });
