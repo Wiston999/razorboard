b7311032fabaa431d0a766197672707b6676afa9
Victor Cabezas <vcabezas@tuenti.com>
2020-11-16 21:31:32 +0100

    Add tests for app.component

--- razorboard/src/app/app.component.spec.ts
+++ razorboard/src/app/app.component.spec.ts
@@ -1,15 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { TopBarComponent } from './top-bar/top-bar.component';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        FontAwesomeModule,
+        HttpClientModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        TopBarComponent,
       ],
     }).compileComponents();
   }));
@@ -24,12 +35,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('RazorBoard');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to RazorBoard!');
   });
 });
